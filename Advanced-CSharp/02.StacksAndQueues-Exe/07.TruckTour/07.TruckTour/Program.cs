eaa82967c391256a6c92c30eff372d98738c03c5
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-21 15:06:03 +0200

    Exe

---
+++ Advanced-CSharp/02.StacksAndQueues-Exe/07.TruckTour/07.TruckTour/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int[]> pumps = new Queue<int[]>();
+
+            int counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] pumpInfo = Console.ReadLine()
+                                        .Split()
+                                        .Select(int.Parse)
+                                        .ToArray();
+
+                pumps.Enqueue(pumpInfo);
+            }
+
+            while (true)
+            {
+                int fuelAmount = 0;
+                bool isFound = true;
+                
+                for (int i = 0; i < n; i++)
+                {
+                    int[] currentPump = pumps.Dequeue();
+                    int currentFuel = currentPump[0];
+                    int currentDistace = currentPump[1];
+
+                    fuelAmount += currentFuel;
+
+                    if (fuelAmount < currentDistace)
+                    {
+                        isFound = false;
+                    }
+
+                    fuelAmount -= currentDistace;
+
+                    pumps.Enqueue(currentPump);
+                }
+
+                if (isFound)
+                {
+                    break;
+                }
+
+                counter++;
+
+                pumps.Enqueue(pumps.Dequeue());
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Advanced-CSharp/02.StacksAndQueues-Exe/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            Stack<char> parentheses = new Stack<char>();
+
+            bool isBalanced = true;
+
+            foreach (char ch in input)
+            {
+                if (ch == '(' || ch == '[' || ch == '{')
+                {
+                    parentheses.Push(ch);
+                }
+                else
+                {
+                    if (parentheses.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+
+                    char openBreckets = parentheses.Pop();
+                    bool isRound = openBreckets == '(' && ch == ')';
+                    bool isSqare = openBreckets == '[' && ch == ']';
+                    bool isCurl = openBreckets == '{' && ch == '}';
+
+                    if (isRound == false && isSqare == false && isCurl == false)
+                    {
+                        isBalanced = false;
+                    }
+                }
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
