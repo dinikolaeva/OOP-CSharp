f2d110b03e7ee531080c9b18ea420303e0ff5c3c
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-21 16:09:17 +0200

    Lab

---
+++ Advanced-CSharp/01.Stacks and Queues-Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            List<int> evenNums = new List<int>();
+
+            while (queue.Count > 0)
+            {
+                queue.Peek();
+                var currentNum = queue.Dequeue();
+
+                if (currentNum % 2 == 0)
+                {
+                    evenNums.Add(currentNum);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", evenNums));
+        }
+    }
+}
