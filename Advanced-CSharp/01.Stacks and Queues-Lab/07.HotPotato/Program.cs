e711f6ba0a7487606adbcc947f4e5d36526f6fd2
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-03-21 14:41:33 +0200

    Lab

---
+++ Advanced-CSharp/01.Stacks and Queues-Lab/07.HotPotato/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            int counting = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<string>();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                queue.Enqueue(names[i]);
+            }
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < counting; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
