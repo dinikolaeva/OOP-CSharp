8f964680d72b5003c916dcd9971ac84f61b7b92c
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2021-04-22 15:16:26 +0300

    Lab

---
+++ OOP-CSharp/05.InterfacesAndAbstraction-Lab/01.Shapes/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Draw()
+        {
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+
+            for (double i = this.radius; i >= -this.radius; --i)
+            {
+                for (double j = -this.radius; j < rOut; j += 0.5)
+                {
+                    double value = i * i + j * j;
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ OOP-CSharp/05.InterfacesAndAbstraction-Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public void Draw()
+        {
+            DrawaLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawaLine(this.width, '*', ' ');
+            }
+            DrawaLine(this.width, '*', '*');
+        }
+        private void DrawaLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
